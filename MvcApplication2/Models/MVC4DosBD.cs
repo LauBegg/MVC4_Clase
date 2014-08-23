77d4a178baae03d0af11292e1720a91745817660
LauBegg <noedelia1989@gmail.com>
2014-08-23 11:58:20 -0400

    MVC4_BD

---
+++ MvcApplication2/Models/MVC4DosBD.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace MvcApplication2.Models
+{
+    //la sgte linea es una herencia
+    public class MVC4DosBD : DbContext
+    {
+       //instanciamos nuestra Base de Datos
+
+        public MVC4DosBD() : base("name = Default Connection") {
+        
+        }
+
+        public DbSet <Restaurant> Restaurants { get; set; }
+
+        public DbSet<RestaurantReview> Reviwes { get; set; }
+    }
+}
